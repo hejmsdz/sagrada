9ae50655422631e9db9279af3f9edf16f3f590d7
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2026-01-01 20:25:05 +0100

    seo improvements

--- src/components/layout/page.tsx
+++ src/components/layout/page.tsx
@@ -1,3 +1,3 @@
 export function Page({ children }: { children: React.ReactNode }) {
-  return <div className="p-4 container mx-auto">{children}</div>;
+  return <main className="p-4 container mx-auto">{children}</main>;
 }
